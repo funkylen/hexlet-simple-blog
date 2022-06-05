a699d49715137a31044770daf360c0ae11d9efa6
funkylen <funkylen@gmail.com>
2022-06-05 13:25:40 +0300

    Update posts

---
+++ resources/views/post/edit.blade.php
@@ -0,0 +1,26 @@
+@extends('layout')
+
+@section('title', 'Edit Post')
+
+@section('content')
+    <form method="post" action="{{ route('update_post', $post->id) }}">
+        @csrf
+
+        <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
+                   aria-describedby="title" value="{{ $post->title }}">
+            <div class="invalid-feedback">
+                @error('title') {{ $message }} @enderror
+            </div>
+        </div>
+
+
+        <div class="mb-3">
+            <label for="content" class="form-label">Content</label>
+            <textarea class="form-control" id="content" rows="6" name="content">{{ $post->content }}</textarea>
+        </div>
+
+        <button type="submit" class="btn btn-success">Submit</button>
+    </form>
+@endsection
